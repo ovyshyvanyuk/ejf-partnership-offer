3ccb30299e0848d469e4c3b8addb831f56e990d4
Oleh Vyshyvaniuk <olegvyshyvanyuk@gmail.com>
2021-03-22 12:09:27 +0200

    added interactive basket

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
 var basket = document.getElementById('basket');
 var basketTemplate = document.getElementById("base-option");
 var basketContains = [];
+var summaryPrice = 6000;
+var summary = document.getElementById("value");
 
 function optionSelect(tag) {
     let optionTag = tag.parentElement;
-    console.log(optionTag.children);
+
     if (tag.checked) {
         let newBasketItem = basketTemplate.cloneNode(true);
-        caption = optionTag.children[0].innerHTML
+        let caption = optionTag.children[0].innerHTML
         newBasketItem.children[0].innerHTML = caption;
-        newBasketItem.children[1].innerHTML = optionTag.children[2].innerHTML;
+        let price = optionTag.children[2].innerHTML;
+        newBasketItem.children[1].innerHTML = price;
+        summaryPrice += Number(price.slice(0, 4));
         newBasketItem.removeAttribute("id");
         basket.appendChild(newBasketItem);
         basketContains.push({ caption, newBasketItem });
-        console.log(basketContains);
     } else {
         let caption = optionTag.children[0].innerText;
-        console.log("Caption:", caption);
         let deleteElement = 0;
         for (let i = 0; i < basketContains.length; i++) {
             if (basketContains[i].caption === caption)
                 deleteElement = basketContains[i];
         }
-        console.log(deleteElement);
+        summaryPrice -= (deleteElement.newBasketItem.getElementsByTagName("span")[0].innerText).slice(0, 4);
         deleteElement.newBasketItem.remove();
     }
+
+    summary.innerText = summaryPrice.toString();
 }
 
 function removeItem(tag) {
     let optionTag = tag.parentElement;
     let caption = optionTag.children[0].innerText;
-    console.log("Caption:", caption);
     let deleteElement = 0;
     for (let i = 0; i < basketContains.length; i++) {
         if (basketContains[i].caption === caption)
             deleteElement = basketContains[i];
     }
-    console.log(deleteElement);
+    summaryPrice -= (deleteElement.newBasketItem.getElementsByTagName("span")[0].innerText).slice(0, 4);
     deleteElement.newBasketItem.remove();
 
     let option = document.getElementById(caption.toLowerCase());
     option.querySelector("div.option-content input.add").checked = false;
+
+    summary.innerText = summaryPrice.toString();
 }
